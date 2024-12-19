4a630a36b91fb98c06b47674838f15ecab2f844b
Joseph Scherreik <joe.scherreik@gmail.com>
2024-12-18 20:35:48 -0500

    add shadcnui

--- app/components/Loading.tsx
+++ app/components/Loading.tsx
@@ -7,9 +7,13 @@ export default function Loading({ limit }: { limit?: number }) {
 /** Renders a popup loader component. */
 export function PopupLoader() {
   return (
-    <div>
-      <h3>Loading...</h3>
-      <i>This will take a couple seconds</i>
+    <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+      <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+        <div className="aspect-video rounded-xl bg-muted/50" />
+        <div className="aspect-video rounded-xl bg-muted/50" />
+        <div className="aspect-video rounded-xl bg-muted/50" />
+      </div>
+      <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
     </div>
   );
 }
