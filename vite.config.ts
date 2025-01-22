6b6601aef1903513680c2620062e6bcd6e5d92e5
Joseph Scherreik <joe@scherreik.com>
2025-01-22 10:17:22 -0500

    Update dependancies and updates (#14)

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { reactRouter } from "@react-router/dev/vite";
-// import { reactRouterDevTools } from "react-router-devtools";
+import { reactRouterDevTools } from "react-router-devtools";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
-    // reactRouterDevTools(),
+    reactRouterDevTools(),
     reactRouter(),
     tsconfigPaths()
   ],
